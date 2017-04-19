9bad4d3ed6d6f81888da6a37186b3c6725fa273e
Dave Rapin <dave@rapin.com>
2017-04-19 18:39:37 -0400

    Started on callbacks and config for omniauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_or_creat_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to root_path
+  end
+
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+end
