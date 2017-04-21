26511d28a4d1e1a0f60bb506c2d6d27a49f99c05
Dave Rapin <dave@rapin.com>
2017-04-21 17:10:52 -0400

    Annotations for company

--- test/factories/companies.rb
+++ test/factories/companies.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  name       :string           not null
+#  tagline    :string
+#  address    :string
+#  logo       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :company do
     email Faker::Internet.unique.email
