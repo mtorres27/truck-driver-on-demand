631290e94154180669fc5a4d9be58c43757e6ebf
Brian Hogg <brian@hoggworks.com>
2017-09-01 10:26:38 -0400

    Added validation to file uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,6 +6,8 @@ class AvatarUploader < Shrine
   plugin :store_dimensions
   plugin :processing
   plugin :default_url
+  plugin :validation_helpers
+  plugin :remove_invalid
 
   process(:store) do |io, context|
     resize_to_limit!(io.download, 300, 300)
@@ -13,5 +15,9 @@ class AvatarUploader < Shrine
 
   Attacher.default_url do
     "#{name}/missing.png"
+  end
+
+  Attacher.validate do
+    validate_mime_type_inclusion %w[image/png image/gif image/jpg image/jpeg]
   end
 end
