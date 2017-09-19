cac150ebd4bd30071fabd7b5093c20e5dbe28f0c
Tamer Ibrahim <tibrahim@tabssoft.com>
2017-09-19 14:57:39 -0400

    Stripe charge first commit (Basic payments with stripe form)

---
+++ app/controllers/company/charges_controller.rb
@@ -0,0 +1,22 @@
+class Company::ChargesController < Company::BaseController
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = 15_000
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
