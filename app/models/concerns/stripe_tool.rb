4451427ab8f1eb2b64a85f8fdc5c832b1bc3b6e5
Tamer Ibrahim <khodeir.mt@gmail.com>
2017-10-01 14:56:48 -0400

    Stripe cancel subscription service

--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -1,13 +1,18 @@
 module StripeTool
   def self.update_company_info(company: company, customer: customer, subscription: subscription)
-    period = subscription.plan.interval == "month" ? 1.month : 1.year
-    company.expires_at = (company.expires_at && company.expires_at > Date.today ? company.expires_at : Date.today) + period
-    company.stripe_customer_id = customer.id
-    company.is_active = true
-    company.last_4_digits = customer.sources.data[0].last4
-    company.card_brand = customer.sources.data[0].brand
-    company.exp_month = customer.sources.data[0].exp_month
-    company.exp_year = customer.sources.data[0].exp_year
+    # period = subscription.plan.interval == "month" ? 1.month : 1.year
+    # company.expires_at = (company.expires_at && company.expires_at > Date.today ? company.expires_at : Date.today) + period
+    company.billing_period_ends_at    = Time.at(subscription.current_period_end).to_datetime
+    company.stripe_customer_id        = customer.id
+    company.stripe_subscription_id    = subscription.id
+    company.stripe_plan_id            = subscription.plan.id
+    company.subscription_cycle        = subscription.plan.interval
+    company.subscription_status       = subscription.status
+    company.is_subscription_cancelled = false
+    company.last_4_digits             = customer.sources.data[0].last4
+    company.card_brand                = customer.sources.data[0].brand
+    company.exp_month                 = customer.sources.data[0].exp_month
+    company.exp_year                  = customer.sources.data[0].exp_year
     company.save
   end
 
@@ -28,7 +33,6 @@ module StripeTool
   end
 
   def self.subscribe(customer: customer, plan_id: plan_id, is_new: is_new, registered_from: registered_from)
-
     customer.subscriptions.create(
       plan: plan_id,
       trial_period_days: (90-registered_from > 0) ? 90-registered_from : 0
@@ -53,6 +57,61 @@ module StripeTool
     # invoice.pay
   end
 
-  def self.connect
+  def self.cancel_subscription(company: company)
+    company.is_subscription_cancelled = true
+    subscription = Stripe::Subscription.retrieve(company.stripe_subscription_id)
+    period_end = self.get_cancel_period_end(subscription: subscription)
+    if subscription.plan.interval == 'month'
+      self.cancel_month(subscription: subscription, period_end: period_end)
+    else
+      self.cancel_year(subscription: subscription, period_end: period_end)
+      # self.refund_customer(subscription: subscription)
+    end
+    company.billing_period_ends_at = Time.at(period_end).to_date
+    company.save
   end
+
+  private
+
+    def self.get_cancel_period_end(subscription: subscription)
+      if (subscription.status == 'trialing' || subscription.plan.interval != 'month') && Time.at(subscription.current_period_start).to_date != Date.today && (subscription.current_period_end - Time.now.to_i) / 86_400 > 31
+        period_end = Time.at(subscription.current_period_end).to_date
+        period_end = period_end >> -1 while period_end >= Date.today
+        return (period_end >> 1).to_time.to_i
+      end
+      if Time.at(subscription.current_period_start).to_date == Date.today
+        return Date.today.next_month.to_time.to_i
+      end
+      return subscription.current_period_end
+    end
+
+    def self.cancel_month(subscription: subscription, period_end: period_end)
+      if subscription.status == 'trialing'
+        subscription.trial_end = period_end
+        subscription.prorate = false
+        subscription.save
+      end
+      subscription.delete(
+        at_period_end: true
+      )
+    end
+
+    def self.refund_customer(subscription: subscription, period_end: period_end)
+      subscription.prorate = false
+      subscription.save
+      subscription.delete(
+        at_period_end: true
+      )
+    end
+
+    def self.cancel_year(subscription: subscription, period_end: period_end)
+      subscription.trial_end = period_end
+      subscription.prorate = false
+      subscription.save
+
+      subscription.delete(
+        at_period_end: false
+      )
+      # Refund the rest of period
+    end
 end
