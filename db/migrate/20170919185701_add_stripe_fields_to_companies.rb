16abd07dd3ced40642b00a51da77714e1a366479
Tamer Ibrahim <khodeir.mt@gmail.com>
2017-10-01 14:54:27 -0400

    Change some stripe columns names in DB

--- db/migrate/20170919185701_add_stripe_fields_to_companies.rb
+++ db/migrate/20170919185701_add_stripe_fields_to_companies.rb
@@ -5,8 +5,8 @@ class AddStripeFieldsToCompanies < ActiveRecord::Migration[5.1]
     add_column :companies, :stripe_plan_id, :string, limit: 20
     add_column :companies, :subscription_cycle, :string, limit: 10
     add_column :companies, :is_subscription_cancelled, :boolean, default: false
-    add_column :companies, :is_active, :boolean, default: false
-    add_column :companies, :expires_at, :datetime
+    add_column :companies, :subscription_status, :string, limit: 10
+    add_column :companies, :billing_period_ends_at, :datetime
     add_column :companies, :last_4_digits, :string, limit: 4
     add_column :companies, :card_brand, :string, limit: 20
     add_column :companies, :exp_month, :string, limit: 2
