9fb331c071bc9c4eb65e60c5f16dd46f4276cea0
Brian Hogg <brian@hoggworks.com>
2017-10-26 10:22:58 -0400

    Scope of work is required, option to hide it removed. Some descriptive text added.

--- app/models/job.rb
+++ app/models/job.rb
@@ -149,6 +149,8 @@ class Job < ApplicationRecord
   validates :pay_type, inclusion: { in: pay_type.values }, allow_blank: true
   validates :freelancer_type, inclusion: { in: freelancer_type.values }
   validates_presence_of :currency
+  validates_presence_of :scope_of_work
+  validates_presence_of :address
 
   def freelancer
     applicants.with_state(:accepted).first&.freelancer
