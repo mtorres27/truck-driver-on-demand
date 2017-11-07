d41b2b04d1ce65b991a4e9c9b9af24cd671dbbc1
Brian Hogg <brian@hoggworks.com>
2017-11-07 17:01:15 -0500

    needed distinct, not uniq, apparently.

--- app/controllers/company/freelancers_controller.rb
+++ app/controllers/company/freelancers_controller.rb
@@ -52,7 +52,7 @@ class Company::FreelancersController < Company::BaseController
 
   def hired
     @locations = current_company.freelancers.uniq.pluck(:area)
-    @freelancers = current_company.freelancers.uniq
+    @freelancers = current_company.freelancers.distinct
 
     if params[:location] && params[:location] != ""
       @freelancers = @freelancers.where({ area: params[:location] })
