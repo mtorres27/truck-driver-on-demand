9acd5822f4d5fd6309514be6280c180e372d063f
Tamer Ibrahim <tibrahim@tabssoft.com>
2017-11-29 23:48:53 -0500

    Add missing variables to emails

--- app/mailers/payments_mailer.rb
+++ app/mailers/payments_mailer.rb
@@ -5,27 +5,29 @@ class PaymentsMailer < ApplicationMailer
     @company = company
     @freelancer = freelancer
     @job = job
-    mail(to: @company.email, subject: @freelancer.name + "has been accepted the work order. It's time to fund the job." )
+    mail(to: @company.email, subject: @freelancer.name + " has been accepted the work order. It's time to fund the job." )
   end
 
-  def request_payout_company(company, freelancer, job)
+  def request_payout_company(company, freelancer, job, payment)
     @company = company
     @freelancer = freelancer
     @job = job
-    mail(to: @company.email, subject: @freelancer.name + "has requested a payout.")
+    @payment = payment
+    mail(to: @company.email, subject: @freelancer.name + " has requested a payout.")
   end
 
   def notice_funds_freelancer(company, freelancer, job)
     @company = company
     @freelancer = freelancer
     @job = job
-    mail(to: @freelancer.email, subject: @company.name + "has funded your job. It's time to start working!")
+    mail(to: @freelancer.email, subject: @company.name + " has funded your job. It's time to start working!")
   end
 
-  def notice_payout_freelancer(company, freelancer, job)
+  def notice_payout_freelancer(company, freelancer, job, payment)
     @company = company
     @freelancer = freelancer
     @job = job
-    mail(to: @freelancer.email, subject: @company.name + "has release a payout.")
+    @payment = payment
+    mail(to: @freelancer.email, subject: @company.name + " has release a payout.")
   end
 end
