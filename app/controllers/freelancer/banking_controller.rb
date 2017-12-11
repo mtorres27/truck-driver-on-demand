7bddb0c8578d363da3d33c13f75844d5cf37aa17
Tamer Ibrahim <tibrahim@tabssoft.com>
2017-12-11 00:43:59 -0500

    Make verification document required in Stripe Identity Verification Form

--- app/controllers/freelancer/banking_controller.rb
+++ app/controllers/freelancer/banking_controller.rb
@@ -18,10 +18,11 @@ class Freelancer::BankingController < Freelancer::BaseController
     type = params[:account][:type]
     @post_data['type'] = type
     if flash[:error].nil? || flash[:error].empty?
+      flash[:error] = 'Please fill all the fields' if params[:account][type][:legal_entity][:verification].nil? || params[:account][type][:legal_entity][:verification][:document].nil?
       params[:account][type][:legal_entity].each do |key, value|
         if [ :address, :dob, :personal_address, :verification ].include? key.to_sym
           value.each do |akey, avalue|
-            flash[:error] = 'Please fill all the fields' if avalue.nil? || (!akey['document'] && avalue.empty?)
+            flash[:error] = 'Please fill all the fields' if avalue.nil?
             @post_data['legal_entity.'+key+'.'+akey] ||= {}
             @post_data['legal_entity.'+key+'.'+akey] = avalue unless akey['document']
           end
