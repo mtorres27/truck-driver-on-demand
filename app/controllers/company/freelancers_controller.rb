c09e2d69cac4be6eeb0c8806770d2de112b266cf
Brian Hogg <brian@hoggworks.com>
2017-12-19 17:44:53 -0500

    Whee

--- app/controllers/company/freelancers_controller.rb
+++ app/controllers/company/freelancers_controller.rb
@@ -49,10 +49,8 @@ class Company::FreelancersController < Company::BaseController
     end
 
     if (!@keywords and !@address) or (@keywords.blank? and @address.blank?)
-      flash[:error] = "Empty"
       @freelancers = Freelancer.none
     else
-      flash[:error] = "not empty"
       @freelancers = @freelancers.search(@keywords)
     end
 
