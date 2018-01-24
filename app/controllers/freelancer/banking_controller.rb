63cd57ef5730e4778da8fc84cd1f39ad3ee196d0
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-01-23 20:55:13 -0500

    Add x-pdf to accepted mime types for verification document upload

--- app/controllers/freelancer/banking_controller.rb
+++ app/controllers/freelancer/banking_controller.rb
@@ -1,9 +1,8 @@
 class Freelancer::BankingController < Freelancer::BaseController
-  DOC_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf']
+  DOC_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf', 'application/x-pdf']
   def index
     @connector = StripeAccount.new(current_freelancer)
-    logger.debug @connector.account.inspect
-    # logger.debug 321
+    # logger.debug @connector.account.inspect
   end
 
   def identity
