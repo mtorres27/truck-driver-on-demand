45f255c114a8bdb5c43ae0b5ca1290f9b6ea7d23
Brian Hogg <brian@hoggworks.com>
2018-01-27 08:53:43 -0500

    Blergh

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -75,7 +75,7 @@ class Applicant < ApplicationRecord
     Applicant.all.each do |applicant|
       applicant.messages.where({has_quote: false}).each do |message|
         applicant.quotes.each do |quote|
-          if (message.created_at - quote.created_at).abs < 2
+          if (message.created_at - quote.created_at).abs < 4
             message.has_quote = true
             message.quote_id = quote.id
             message.save
