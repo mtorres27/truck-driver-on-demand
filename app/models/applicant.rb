7d9c0b938596a8360c32602b10215f7c21647090
Brian Hogg <brian@hoggworks.com>
2018-01-27 08:51:40 -0500

    Broadening

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -75,7 +75,7 @@ class Applicant < ApplicationRecord
     Applicant.all.each do |applicant|
       applicant.messages.where({has_quote: false}).each do |message|
         applicant.quotes.each do |quote|
-          if (message.created_at - quote.created_at).abs < 1
+          if (message.created_at - quote.created_at).abs < 2
             message.has_quote = true
             message.quote_id = quote.id
             message.save
