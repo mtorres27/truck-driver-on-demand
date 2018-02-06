d7c7f63c3d8ffca21025ece69548afbfad1189da
Manfred Torres <manfredtorress@gmail.com>
2018-02-06 11:10:28 -0500

    Bugfix Datatables

--- app/assets/javascripts/controllers/admin/companies.js
+++ app/assets/javascripts/controllers/admin/companies.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
+$(document).on("turbolinks:load", function(){
     $('#admin-companies-table').dataTable({
+        searching: false,
+        paging: false,
+        info: false,
         columnDefs: [ {
             targets: [5],
             orderable: false
