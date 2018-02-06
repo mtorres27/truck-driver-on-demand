baf00ef127cb91e81080e7682107e8bf33490b71
Manfred Torres <manfredtorress@gmail.com>
2018-02-06 11:10:28 -0500

    Bugfix Datatables

--- app/assets/javascripts/controllers/admin/freelancers.js
+++ app/assets/javascripts/controllers/admin/freelancers.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
+$(document).on("turbolinks:load", function(){
     $('#admin-freelancers-table').dataTable({
+        searching: false,
+        paging: false,
+        info: false,
         columnDefs: [ {
             targets: [5],
             orderable: false
