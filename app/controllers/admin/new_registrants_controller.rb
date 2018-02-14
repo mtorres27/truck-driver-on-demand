e6f279d4b4597aec32af7aa800615819e3db8697
Manfred Torres <manfredtorress@gmail.com>
2018-02-14 12:51:04 -0500

    Add datatables to new registrants and sort by date created

--- app/controllers/admin/new_registrants_controller.rb
+++ app/controllers/admin/new_registrants_controller.rb
@@ -3,6 +3,6 @@ class Admin::NewRegistrantsController < Admin::BaseController
   def index
     freelancers = Freelancer.where(disabled: true)
     companies = Company.where(disabled: true)
-    @new_registrants = (freelancers + companies).sort_by { |registrant| registrant.name }
+    @new_registrants = (freelancers + companies).sort_by { |registrant| registrant.created_at }.reverse
   end
 end
