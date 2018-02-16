5367a5b35b41a3d7fcdc6e67f21eb21223fadbe0
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-02-16 16:46:43 -0500

    Handle exception on local for not finising the ids

--- app/controllers/company/main_controller.rb
+++ app/controllers/company/main_controller.rb
@@ -2,7 +2,12 @@ class Company::MainController < Company::BaseController
 
   def index
     # @freelancers = Freelancer.where(disabled: false).order(id: 'DESC').limit(4)
-    @freelancers = Freelancer.find([335, 229, 320, 393]);
+    @freelancers ||= []
+    begin
+      @freelancers = Freelancer.find([335, 229, 320, 393]);
+    rescue Exception
+
+    end
   end
 
 end
--- app/controllers/freelancer/main_controller.rb
+++ app/controllers/freelancer/main_controller.rb
@@ -1,7 +1,12 @@
 class Freelancer::MainController < Freelancer::BaseController
   def index
     # @companies =  Company.where(:disabled => false).order(id: 'DESC').limit(4)
-    @companies = Company.find([67, 13, 64, 59]);
-    logger.debug @companies.inspect
+    # @companies = Company.find([67, 13, 64, 59]);
+    @companies ||= []
+    begin
+      @companies = Company.find([335, 229, 320, 393]);
+    rescue Exception
+
+    end
   end
 end
