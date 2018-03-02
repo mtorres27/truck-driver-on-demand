70959b8e1dbf90fc7e3870ec90dc0feea8b24d94
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-03-02 13:58:01 -0500

    Admin edit freelancer ( same frontend form)

--- app/controllers/admin/freelancers_controller.rb
+++ app/controllers/admin/freelancers_controller.rb
@@ -89,7 +89,13 @@ class Admin::FreelancersController < Admin::BaseController
         job_markets: (I18n.t("enumerize.freelancer_live_events_staging_and_rental_job_markets").keys + I18n.t("enumerize.freelancer_system_integration_job_markets").keys).uniq,
         job_functions: (I18n.t("enumerize.freelancer_system_integration_job_functions").keys + I18n.t("enumerize.freelancer_live_events_staging_and_rental_job_functions").keys).uniq,
         technical_skill_tags:  I18n.t("enumerize.freelancer_technical_skill_tags").keys,
-        manufacturer_tags:  I18n.t("enumerize.freelancer_manufacturer_tags").keys
+        manufacturer_tags:  I18n.t("enumerize.freelancer_manufacturer_tags").keys,
+        certifications_attributes: [:id, :certificate, :cert_type, :name, :_destroy],
+        freelancer_references_attributes: [:id, :title, :description, :phone, :name, :_destroy],
+        freelancer_affiliations_attributes: [:id, :name, :image, :_destroy],
+        freelancer_insurances_attributes: [:id, :name, :description, :_destroy],
+        freelancer_clearances_attributes: [:id, :description, :image, :_destroy],
+        freelancer_portfolios_attributes: [:id, :name, :image, :_destroy],
       )
 
     end
