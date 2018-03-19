4dc86274e09180d0226a1b64db18795a0b2c17c4
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-03-19 11:53:17 -0400

    Company avj fees invoice

--- app/controllers/company/jobs_controller.rb
+++ app/controllers/company/jobs_controller.rb
@@ -1,6 +1,21 @@
 class Company::JobsController < Company::BaseController
-  before_action :set_job, only: [:show, :edit, :update, :destroy, :contract, :edit_contract, :update_contract, :publish]
+  before_action :set_job, only: [:print_avj_invoice, :avj_invoice, :show, :edit, :update, :destroy, :contract, :edit_contract, :update_contract, :publish]
   before_action :set_company, only: [:edit, :new, :create, :update]
+
+
+  def job_countries
+    country = params[:country]
+    specs = Stripe::CountrySpec.retrieve(country.upcase)
+    render partial: "job_currencies", locals: { currencies: specs.supported_bank_account_currencies } if specs.supported_bank_account_currencies
+  end
+
+  def avj_invoice
+
+  end
+
+  def print_avj_invoice
+
+  end
 
   def new
     @job = Job.new(project_id: params[:project_id])
