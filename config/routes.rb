6245a6d31bda17d4fda3665337ce311fbb2093ed
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-04-02 15:20:43 -0400

    Create Admin pages for plans

--- config/routes.rb
+++ config/routes.rb
@@ -152,6 +152,11 @@ Rails.application.routes.draw do
       get :enable, on: :member
     end
 
+    resources :plans, except: [:new, :create] do
+      get :disable, on: :member
+      get :enable, on: :member
+    end
+
     resources :pages, only: [:index, :edit, :update]
     resources :audits, only: [:index]
 
