7e58a5670bf619c579c1f4404af02bb263f15cc4
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-04-10 14:37:20 -0400

    Small edits and forgotten HEAD

--- app/controllers/company/jobs_controller.rb
+++ app/controllers/company/jobs_controller.rb
@@ -1,5 +1,4 @@
 class Company::JobsController < Company::BaseController
-<<<<<<< HEAD
   before_action :set_job, only: [:print_avj_invoice, :avj_invoice, :show, :edit, :update, :destroy, :contract, :edit_contract, :update_contract, :publish]
   before_action :set_company, only: [:edit, :new, :create, :update]
 
