8be93a710bf1b4f6f6743a4ec694cc0117e31dcb
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-04-25 15:02:58 -0400

    Add original plan fees and fix calculations

--- app/controllers/freelancer/contracts_controller.rb
+++ app/controllers/freelancer/contracts_controller.rb
@@ -17,22 +17,27 @@ class Freelancer::ContractsController < Freelancer::BaseController
 
     # Get Amount in USD
     job_amout = @accepted_quote.amount / currency_rate
-    if @job.company.waived_jobs.positive?
-      plan_fees = 0
-      @job.company.waived_jobs = @job.company.waived_jobs - 1
-      @job.company.save
-    else
-      plan = @job.company.plan
+
+    plan = @job.company.plan
       if job_amout > 2000
         fees = plan.fee_schema['above_2000']
       else
         fees = plan.fee_schema['below_2000']
       end
+
+    if @job.company.waived_jobs.positive?
+      plan_fees = 0
+      @job.company.waived_jobs = @job.company.waived_jobs - 1
+      @job.company.save
+    else
       plan_tax = @job.company.country == 'ca' ? 1.13 : 1
       plan_fees = ( fees.to_i * currency_rate ) * plan_tax
     end
     @job.company_plan_fees = plan_fees
     @job.save
+
+    @accepted_quote.plan_fee = fees
+    @accepted_quote.save
 
     # Send notice email
     PaymentsMailer.request_funds_company(@job.company, current_freelancer, @job).deliver
