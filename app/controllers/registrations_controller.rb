a4aebd13c8abd886a054e57304f3fb3adc45ddc3
Jordan Wong <jwong@pernix-solutions.com>
2018-05-10 12:03:41 -0600

    fix registration bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+  def after_sign_up_path_for(resource)
+    sign_out(resource)
+    if resource.is_a?(Freelancer)
+      session[:step_freelancer_id] = resource.id
+      return freelancer_registration_steps_path
+    end
+    super
+  end
+
   def after_inactive_sign_up_path_for(resource)
     cookies.delete(:onboarding)
     "/confirm_email"
