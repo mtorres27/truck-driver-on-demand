7bff0825aaa70958f8164121381e21756f940baa
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-07-30 12:49:23 -0400

    Fix company fee calculations bug

--- app/controllers/freelancer/contracts_controller.rb
+++ app/controllers/freelancer/contracts_controller.rb
@@ -8,6 +8,9 @@ class Freelancer::ContractsController < Freelancer::BaseController
   def accept
     @job = Job.find(params[:id])
     authorize @job
+
+    # render :show if @job.state == "contracted"
+
     @job.state = "contracted"
     @job.contracted_at = Time.zone.today
     @job.save
@@ -17,7 +20,7 @@ class Freelancer::ContractsController < Freelancer::BaseController
     currency_rate = currency.nil? ?  1 : currency.rate
 
     # Get Amount in USD
-    job_amout = @accepted_quote.amount / currency_rate
+    job_amount = @accepted_quote.amount / currency_rate
 
     fees = @job.company.plan.fee_schema['company_fees']
       # if job_amout > 2000
@@ -27,12 +30,20 @@ class Freelancer::ContractsController < Freelancer::BaseController
       # end
 
     if @job.company.waived_jobs.positive?
+      logger.debug "Waived job"
       plan_fees = 0
       @job.company.waived_jobs -= 1
       @job.company.save
     else
       plan_tax = @job.company.canada_country? ? 1 + (Subscription::CANADA_SALES_TAX_PERCENT/100) : 1
-      plan_fees = ( fees.to_i / 100 * job_amout ) * plan_tax
+      plan_fees =  fees.to_f / 100 * job_amount.to_f  * plan_tax
+      logger.debug "Paid job"
+      logger.debug fees.to_i
+      logger.debug job_amount.to_f
+      logger.debug (fees.to_f / 100)
+      logger.debug ( fees.to_i / 100 * job_amount.to_f )
+      logger.debug ( fees.to_i / 100 * job_amount.to_f ) * plan_tax
+      logger.debug plan_fees
     end
     @job.company_plan_fees = plan_fees
     @job.save
