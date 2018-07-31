c47ffb1915faef73f3b31907d7174612fa0a8052
Tamer Ibrahim <tibrahim@tabssoft.com>
2018-07-31 19:24:06 -0400

    Fix migration change issue when deploying to dev

--- db/migrate/20180728165546_drop_quotes.rb
+++ db/migrate/20180728165546_drop_quotes.rb
@@ -4,12 +4,14 @@ class DropQuotes < ActiveRecord::Migration[5.1]
       if job.applicants.with_state(:accepted).count > 0
         accepted_applicant = job.applicants.with_state(:accepted).first
         accepted_quote = execute("select * from quotes where state = 'accepted' and applicant_id = #{accepted_applicant.id}").first
-        job.update_attributes(total_amount: accepted_quote['total_amount'],
-                              tax_amount: accepted_quote['tax_amount'],
-                              stripe_fees: accepted_quote['stripe_fees'],
-                              plan_fee: accepted_quote['plan_fee'],
-                              amount_subtotal: accepted_quote['amount']
-        )
+        if accepted_quote
+          job.update_attributes(total_amount: accepted_quote['total_amount'],
+                                tax_amount: accepted_quote['tax_amount'],
+                                stripe_fees: accepted_quote['stripe_fees'],
+                                plan_fee: accepted_quote['plan_fee'],
+                                amount_subtotal: accepted_quote['amount']
+          )
+        end
       end
     end
 
