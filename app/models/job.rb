dcf5736b1f4f9c686e74e95fa686615c38ce7c19
Manfred Torres <manfredtorress@gmail.com>
2018-08-06 15:24:42 -0500

    Add edit work order button when saved as draft

--- app/models/job.rb
+++ app/models/job.rb
@@ -126,6 +126,7 @@ class Job < ApplicationRecord
     :published,
     :quoted,
     :negotiated,
+    :contract_draft,
     :contracted,
     :completed
   ], predicates: true, scope: true
@@ -168,12 +169,16 @@ class Job < ApplicationRecord
     %w(created published quoted).include?(state)
   end
 
+  def work_order_in_draft?
+    pre_negotiated? && freelancer.present? && !contract_sent?
+  end
+
   def is_published?
     state != :created
   end
 
   def pre_contracted?
-    pre_negotiated? || negotiated?
+    pre_negotiated? || work_order_in_draft?
   end
 
   def work_order
