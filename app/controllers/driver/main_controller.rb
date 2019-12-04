1d8b6c5eaf8bb7dae827b64d16c568f434d79d8d
mtorres27 <manfred.torres.s@gmail.com>
2019-12-04 09:11:05 -0500

    88-user-onboarding-homescreen

--- app/controllers/driver/main_controller.rb
+++ app/controllers/driver/main_controller.rb
@@ -3,6 +3,7 @@
 class Driver::MainController < Driver::BaseController
 
   before_action :redirect_to_registration_step, if: :current_driver_registering?
+  before_action :redirect_to_onboarding_process
 
   def index
     authorize current_user
@@ -15,6 +16,10 @@ class Driver::MainController < Driver::BaseController
     return if current_user.registration_completed?
 
     redirect_to driver_registration_step_path(current_user.driver_profile&.registration_step)
+  end
+
+  def redirect_to_onboarding_process
+    redirect_to driver_onboarding_process_index_path
   end
 
 end
